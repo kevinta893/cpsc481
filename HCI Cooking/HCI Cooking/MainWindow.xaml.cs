964c7d836a38c598b275027ef538443e9d6f3a7f
kevinta893 <kevinta893@gmail.com>
2014-03-11 19:13:44 -0600

    For now?

---
+++ HCI Cooking/HCI Cooking/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+
+namespace HCI_Cooking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Random r = new Random();
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            label1.Margin = new Thickness(r.Next(400), r.Next(230), 0, 0);
+       
+        }
+
+
+       
+        }
+    
+    
+}
