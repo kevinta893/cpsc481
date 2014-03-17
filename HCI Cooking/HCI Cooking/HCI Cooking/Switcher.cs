3e79961e62c6307b568e4f1cd29b374638008368
kevinta893 <kevinta893@gmail.com>
2014-03-17 16:16:00 -0600

    Page designs updated
    Page designs updated
    
    Adds switcher backtrack method. Please use these methods from now on.

---
+++ HCI Cooking/HCI Cooking/HCI Cooking/Switcher.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Code taken from the following website. With slight modifications
+ * 
+ * http://azerdark.wordpress.com/2010/04/23/multi-page-application-in-wpf/
+ */
+
+
+
+using System.Windows.Controls;
+using System.Windows;
+using HCI_Cooking;
+
+namespace HCI_Cooking
+{
+    public static class Switcher
+    {
+        public static MainWindow pageSwitcher;
+
+        public static void Switch(UserControl newPage)
+        {
+            pageSwitcher.Navigate(newPage);
+        }
+
+        public static void Switch(UserControl newPage, object state)
+        {
+            pageSwitcher.Navigate(newPage, state);
+        }
+    }
+}
