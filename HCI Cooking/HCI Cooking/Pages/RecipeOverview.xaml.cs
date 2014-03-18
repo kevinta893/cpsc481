63c221478120bcc2854d9e9c195a5f7e9b110140
peterbuk8 <peterbuk8@gmail.com>
2014-03-18 17:56:15 -0600

    made recipe overview more dynamic.

--- HCI Cooking/HCI Cooking/Pages/RecipeOverview.xaml.cs
+++ HCI Cooking/HCI Cooking/Pages/RecipeOverview.xaml.cs
@@ -19,10 +19,42 @@ namespace HCI_Cooking.Pages
     /// </summary>
     public partial class RecipeOverview : ISwitchable
     {
-        public RecipeOverview()
+        Recipe currentRecipe;
+
+        public RecipeOverview(Recipe rec)
         {
             InitializeComponent();
+
+            currentRecipe = rec;
+
+            LoadRecipeValues();
         }
+
+        // load in the values from the recipe
+        private void LoadRecipeValues()
+        {
+            lblTitle.Content = currentRecipe.Title;
+
+            // display ingredients list
+            for (int i = 0; i < currentRecipe.Ingredients.Count(); i++)
+            {
+                txtBlkIngred.Text += "\n" + currentRecipe.Ingredients[i];
+            }
+
+            // disaply tools list
+            for (int i = 0; i < currentRecipe.Tools.Count(); i++)
+            {
+                txtBlkTools.Text += "\n" + currentRecipe.Tools[i];
+            }
+
+            // display description
+            txtBlkDescr.Text = currentRecipe.Description;
+           
+
+        }
+
+
+        //********************** Event handlers below *******
 
 
         #region ISwitchable Members
