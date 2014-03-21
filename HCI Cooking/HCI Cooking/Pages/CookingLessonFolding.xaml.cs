43535f58dd2d675996c9d56168d009e0e138ff65
kevinta893 <kevinta893@gmail.com>
2014-03-20 21:44:10 -0600

    Added folding description.

--- HCI Cooking/HCI Cooking/Pages/CookingLessonFolding.xaml.cs
+++ HCI Cooking/HCI Cooking/Pages/CookingLessonFolding.xaml.cs
@@ -25,6 +25,13 @@ namespace HCI_Cooking.Pages
         {
             currentRecipe = rec;
             InitializeComponent();
+
+            txtBlkChapOverview.Text = "Folding is the process of both removing air bubbles from\r\n your dough and mixing ingredients together. " +
+                "This is a \r\ndifferent process from mixing because removing air bubbles\r\nrequires a different mixing technique for ingredients that\r\ntend to froth more if mixed intensely.\r\n\r\n" + 
+                "In this tutorial, you will learn how to: \r\n" +
+                "- Why we remove bubbles from your baking mixture\r\n" +
+                "- The technique of removing air bubbles from your mixture\r\n" +
+                "- Advanced tips\r\n";
         }
 
 
