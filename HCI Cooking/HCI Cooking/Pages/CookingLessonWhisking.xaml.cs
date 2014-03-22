bf679edc4c24345fda7a6912988d1f982bf5a819
liendroid <littleassassin_001@hotmail.com>
2014-03-22 00:44:26 -0600

    Dynamic Video Lessons
    Not Implemented Lessons Template is complete. Folding and whisking have
    dynamic lesson tables. Lesson arrows and lesson learnt symbols have been
    implemented.
    
    Error: Lesson Label does not appear. Someone please fix.

--- HCI Cooking/HCI Cooking/Pages/CookingLessonWhisking.xaml.cs
+++ HCI Cooking/HCI Cooking/Pages/CookingLessonWhisking.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace HCI_Cooking.Pages
 {
@@ -19,13 +20,13 @@ namespace HCI_Cooking.Pages
     /// </summary>
     public partial class CookingLessonWhisking : UserControl
     {
-        Recipe aRecipe;
+        Recipe currentRecipe;
         Database userDb;
         User mainUser; 
 
         public CookingLessonWhisking(Recipe rec)
         {
-            aRecipe = rec;
+            currentRecipe = rec;
             userDb = Database.getInstance();
             mainUser = userDb.userList[0];
 
@@ -40,8 +41,111 @@ namespace HCI_Cooking.Pages
                     "- Not spill your ingredients\r\n"+
                     "- Use other kitchen tools to whisk\r\n" +
                     "- Determine dough consistancy";
-           
+            loadLessonTable();
         }
+
+        private void loadLessonTable()
+        {
+            Grid lessonGrid;
+            System.Windows.Controls.Image lessonImg = null;
+            Label lessonLabel = null;
+            Button lessonButton = null;
+
+            for (int i = 0; i < currentRecipe.Skills.Count(); i++)
+            {
+                lessonGrid = (Grid)grdLessonsTable.Children[i];
+
+                for (int item = 0; item < lessonGrid.Children.Count; item++)
+                {
+                    if (lessonGrid.Children[item] is System.Windows.Controls.Image)
+                    {
+                        lessonImg = (System.Windows.Controls.Image)lessonGrid.Children[item];
+                    }
+                    else if (lessonGrid.Children[item] is Label)
+                    {
+                        lessonLabel = (Label)lessonGrid.Children[item];
+                    }
+                    else if (lessonGrid.Children[item] is Button)
+                    {
+                        lessonButton = (Button)lessonGrid.Children[item];
+                        if (currentRecipe.Skills[i] == "Whisking")
+                        {
+                            lessonButton.Visibility = Visibility.Hidden;
+                        }
+                    }
+
+                }
+                grdLessonsTable.Children[i].Visibility = Visibility.Visible;
+
+                lessonImg.Source = ImageLoader.ToWPFImage(new Bitmap(HCI_Cooking.Properties.Resources.red_cross));
+                lessonLabel.Content = currentRecipe.Skills[i];
+
+                for (int k = 0; k < mainUser.KnownSkills.Count; k++)
+                {
+                    if (currentRecipe.Skills[i] == mainUser.KnownSkills[k])
+                    {
+                        lessonImg.Source = ImageLoader.ToWPFImage(new Bitmap(HCI_Cooking.Properties.Resources.green_check));
+                    }
+                }
+
+                //if the current skill is equal to folding, show an arrow pointing to it.
+                if (currentRecipe.Skills[i] == "Whisking")
+                {
+                    lessonImg.Source = ImageLoader.ToWPFImage(new Bitmap(HCI_Cooking.Properties.Resources.arrow_right));
+                }
+
+                if (currentRecipe.Skills[i].Equals("Folding"))
+                {
+                    lessonButton.Click += new RoutedEventHandler(lessonButtonFold_Click);
+                }
+                else
+                {
+                    lessonButton.Click += new RoutedEventHandler(lessonButtonNI_Click);
+                }
+            }
+        }
+
+        private void  lessonButtonNI_Click(object sender, RoutedEventArgs e)
+        {
+ 	        bool hasLearntLesson = false;
+
+            foreach (string skill in mainUser.KnownSkills)
+            {
+                if (skill == "Whisking")
+                {
+                    hasLearntLesson = true;
+                }
+            }
+
+            if (hasLearntLesson == false)
+            {
+                mainUser.KnownSkills.Add("Whisking");
+                mainUser.LessonsLearnt += 1;
+            }
+
+            Switcher.Switch(new CookingLessonTemplate(currentRecipe));
+        } 
+
+        private void  lessonButtonFold_Click(object sender, RoutedEventArgs e)
+        {   
+    	   bool hasLearntLesson = false;
+
+            foreach (string skill in mainUser.KnownSkills)
+            {
+                if (skill == "Whisking")
+                {
+                    hasLearntLesson = true;
+                }
+            }
+
+            if (hasLearntLesson == false)
+            {
+                mainUser.KnownSkills.Add("Whisking");
+                mainUser.LessonsLearnt += 1;
+            }
+
+            Switcher.Switch(new CookingLessonFolding(currentRecipe));
+        } 
 
         private void btnLesBack_Click(object sender, RoutedEventArgs e)
         {
@@ -61,28 +165,8 @@ namespace HCI_Cooking.Pages
                 mainUser.LessonsLearnt += 1;
             }
 
-            Switcher.Switch(new RecipeOverview(aRecipe));
+            Switcher.Switch(new RecipeOverview(currentRecipe));
         }
 
-        private void imgLessonIII_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            bool hasLearntLesson = false;
-
-            foreach (string skill in mainUser.KnownSkills)
-            {
-                if (skill == "Whisking")
-                {
-                    hasLearntLesson = true;
-                }
-            }
-
-            if (hasLearntLesson == false)
-            {
-                mainUser.KnownSkills.Add("Whisking");
-                mainUser.LessonsLearnt += 1;
-            }
-
-            Switcher.Switch(new CookingLessonFolding(aRecipe));
-        }
     }
 }
